12282ea99fd6bedc20bb0796a6b8abc78a2d3f6b
ChanduniNethmini <nchanduni21@gmail.com>
2022-06-04 13:49:34 +0530

    Unit test added

---
+++ frontend/src/components/AdminHome.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class AdminHome extends Component {
+  render() {
+    return (
+      <div data-testid='AdminHome-1'>AdminHome</div>
+    )
+  }
+}
---
+++ frontend/src/components/CreateAdminHome.js
@@ -0,0 +1,9 @@
+import React, { Component } from 'react'
+
+export default class CreateAdminHome extends Component {
+  render() {
+    return (
+      <div data-testid='CreateAdmin-1'>CreateAdminHome</div>
+    )
+  }
+}
